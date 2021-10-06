4e783d26f4854a0d41ef6f8474a5baa10539e912
Brad Walker <brad@thedigilabs.com>
2021-10-06 08:00:07 -0400

    DI refactoring

--- src/TeslaInventory.cs
+++ src/TeslaInventory.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using RestSharp;
 using System;
@@ -15,11 +16,18 @@ namespace TeslaInventoryNet
         // The public Tesla inventory API
         private static readonly string TESLA_API = "https://www.tesla.com/inventory/api/v1/inventory-results";
 
+        private readonly ILogger<TeslaInventory> logger;
+
         /// <summary>
         /// Delegate to process all of the search results at once
         /// </summary>
         /// <param name="results">A <c>IList<Result></c> collection</param>
         public delegate void ResultsAction(IList<Result> results);
+
+        public TeslaInventory(ILogger<TeslaInventory> logger)
+        {
+            this.logger = logger;
+        }
 
         /// <summary>
         /// Performs a search based on the provided filter criteria.
@@ -27,7 +35,7 @@ namespace TeslaInventoryNet
         /// <param name="location">The geographic location to search</param>
         /// <param name="criteria">The search criteria to use</param>
         /// <param name="action">The delegate to process results</param>
-        public static void Search(Location location, SearchCriteria criteria, ResultsAction action)
+        public void Search(Location location, SearchCriteria criteria, ResultsAction action)
         {
             action(Search(location, criteria));
         }
@@ -38,10 +46,10 @@ namespace TeslaInventoryNet
         /// <param name="location">The geographic location to search</param>
         /// <param name="criteria">The search criteria to use</param>
         /// <returns>A list of search results</returns>
-        public static IList<Result> Search(Location location, SearchCriteria criteria)
+        public IList<Result> Search(Location location, SearchCriteria criteria)
         {
             // build the querystring
-            var query = new {
+            var query = JsonConvert.SerializeObject(new {
                 query = new {
                     model = criteria.Model,
                     condition = criteria.Condition,
@@ -50,12 +58,14 @@ namespace TeslaInventoryNet
                     language = location.Language,
                     region = location.Region
                 }
-            };
+            });
+
+            logger.LogDebug($"Query: {query}");
 
             // Perform the API call
             var client = new RestClient(TESLA_API);
             var request = new RestRequest();
-            request.AddParameter("query", JsonConvert.SerializeObject(query));
+            request.AddParameter("query", query);
             var response = client.Execute(request);
 
             if (response == null)
@@ -63,15 +73,24 @@ namespace TeslaInventoryNet
                 throw new Exception("Error calling Tesla API - null response");
             }
 
-            // Check for API errors first
-            var error = JsonConvert.DeserializeAnonymousType(response.Content, new { Error = "", Code = 0});
-            if (error.Code > 0)
+            if (response.StatusCode != HttpStatusCode.OK)
             {
-                throw new Exception($"Error calling Tesla API - {error.Code}: {error.Error} - {JsonConvert.SerializeObject(query)}");
+                logger.LogDebug($"StatusCode: {response.StatusCode} - {response.ErrorMessage}");
+                throw new Exception(response.ErrorMessage);
             }
+            else 
+            {
+                // Check for API errors first
+                var error = JsonConvert.DeserializeAnonymousType(response.Content, new { Error = "", Code = 0});
+                if (error.Code > 0)
+                {
+                    logger.LogDebug($"Error: {error.Code} - {error.Error}");
+                    throw new Exception($"Error calling Tesla API - {error.Code}: {error.Error} - {JsonConvert.SerializeObject(query)}");
+                }
 
-            // Deserialize the actual results and return the relevant portion
-            return JsonConvert.DeserializeAnonymousType(response.Content, new { results = new Result[0], total_matches_found = 0}).results;
+                // Deserialize the actual results and return the relevant portion
+                return JsonConvert.DeserializeAnonymousType(response.Content, new { results = new Result[0], total_matches_found = 0}).results;
+            }
         }
     }
 }
--- test/UnitTests.cs
+++ test/UnitTests.cs
@@ -1,27 +1,42 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TeslaInventoryNet;
+using Moq;
+using Microsoft.Extensions.Logging;
 
 namespace TeslaInventoryNet.Test
 {
     [TestClass]
     public class UnitTest1
     {
+        private TeslaInventory tesla = null;
+
+        private ILogger<TeslaInventory> CreateLogger()
+        {
+            return new Mock<ILogger<TeslaInventory>>().Object;
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            tesla = new TeslaInventory(CreateLogger());
+        }
+
         [TestMethod]
         public void Search_US_New_NotNull()
         {
-            Assert.IsNotNull(TeslaInventory.Search(Location.US, new SearchCriteria() { Model = "m3", Condition = "new"}));
+            Assert.IsNotNull(tesla.Search(Location.US, new SearchCriteria() { Model = "m3", Condition = "new"}));
         }
 
         [TestMethod]
         public void Search_US_Used_NotNull()
         {
-            Assert.IsNotNull(TeslaInventory.Search(Location.US, new SearchCriteria() { Model = "m3", Condition = "used"}));
+            Assert.IsNotNull(tesla.Search(Location.US, new SearchCriteria() { Model = "m3", Condition = "used"}));
         }
 
         [TestMethod]
         public void Search_US_New_Delegate_NotNull()
         {
-            TeslaInventory.Search(Location.US, new SearchCriteria() { Model = "m3", Condition = "new"}, (results) => {
+            tesla.Search(Location.US, new SearchCriteria() { Model = "m3", Condition = "new"}, (results) => {
                 Assert.IsNotNull(results);
             });
         }
@@ -29,13 +44,13 @@ namespace TeslaInventoryNet.Test
         [TestMethod]
         public void Search_Invalid_Model()
         {
-            Assert.IsNotNull(TeslaInventory.Search(Location.US, new SearchCriteria() { Model = "foo", Condition = "used"}));
+            Assert.IsNotNull(tesla.Search(Location.US, new SearchCriteria() { Model = "foo", Condition = "used"}));
         }
 
         [TestMethod]
         public void Search_Invalid_Condition()
         {
-            Assert.IsNotNull(TeslaInventory.Search(Location.US, new SearchCriteria() { Model = "m3", Condition = "foo"}));
+            Assert.IsNotNull(tesla.Search(Location.US, new SearchCriteria() { Model = "m3", Condition = "foo"}));
         }
     }
 }
